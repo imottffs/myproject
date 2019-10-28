1a8e8d3e9a3e2d7c9a7d7e7983bd8176d15078e8
imottffs <56927403+imottffs@users.noreply.github.com>
2019-10-28 22:16:00 +0800

    Initial commit

---
+++ src/main/kotlin/com/aa/Hello.kt
@@ -0,0 +1,13 @@
+package com.aa
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human{
+    fun hello(){
+        println("Hello kotlin!")
+    }
+}
