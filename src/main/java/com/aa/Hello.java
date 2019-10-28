7f3133d526849114e502dd73d2af254a10435a74
imottffs <56927403+imottffs@users.noreply.github.com>
2019-10-28 23:05:53 +0800

    Java and Kotlin variable, data typ

--- src/main/java/com/aa/Hello.java
+++ src/main/java/com/aa/Hello.java
@@ -6,5 +6,15 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 =19;
+        char c = 'A';
+        Character c2 = c;
+        byte b = 120;
+        float weight =66.5f;//等號的右邊是double 等號的左邊是浮點數 ，要用f
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll =false;
+        String name = "Tom";
     }
 }
--- src/main/kotlin/com/aa/Hello.kt
+++ src/main/kotlin/com/aa/Hello.kt
@@ -5,6 +5,12 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age  = 19;//kotlin 會針對等號右邊的資料型態，來推斷變數型態
+    age = 20
+    var weight =66.5
+    var name : String
+    name ="Hnak"
+
 }
 class Human{
     fun hello(){
